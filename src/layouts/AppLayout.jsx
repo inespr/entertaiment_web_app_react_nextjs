afffbc718dcdb0939d7b165adfa79f29dd6a2105
GONZOLOL <glerdie489@g.educaand.es>
2023-04-11 12:22:15 +0200

    main changes

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,9 +1,15 @@
 import Head from "next/head";
+import SideBar from "@/components/SideBar";
+import SearchBar from "@/components/SearchBar";
 
 export default function AppLayout() {
   return (
     <>
-     <Head></Head>
+      <Head></Head>
+      <div>
+        <SideBar />
+        <SearchBar />
+      </div>
     </>
-  )
+  );
 }
