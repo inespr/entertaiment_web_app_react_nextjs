ae5b866c4f5fd1d513a740a03733c118adb02cf2
inespr <iperrom@g.educaand.es>
2023-04-11 12:29:02 +0200

    ADD CSS MODULES

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,9 +1,14 @@
 import Head from "next/head";
+import styles from '../styles/Layout.module.css'
+import SearchBar from "@/components/SearchBar";
+
 
 export default function AppLayout() {
   return (
-    <>
-     <Head></Head>
-    </>
+    <div className={styles.wrapper}>
+     <Head>
+      <SearchBar/>
+     </Head>
+    </div>
   )
 }
