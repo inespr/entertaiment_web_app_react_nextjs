f8f76655cb7212faed8cfc408e034524c5118ea3
inespr <iperrom@g.educaand.es>
2023-04-17 13:53:20 +0200

    Add fetch call top rated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
+import TopRated from "@/components/Top_rated";
 import Trending from "@/components/Trending";
 import AppLayout from "@/layouts/AppLayout";
 
-function Home({ trendings }) {
+function Home({ trendings, topmovie, toptv }) {
   return (
     <AppLayout
       metaTitle="Entertainment Web App | Home"
       metaDescription="Frontenmentor project realised by...."
     >
       <Trending trendings={trendings} />
+      <TopRated topmovie={topmovie} toptv={toptv}/>
     </AppLayout>
   );
 }
@@ -20,10 +22,24 @@ export async function getStaticProps() {
   );
   const trendings = await response.json();
   console.log("query trending", trendings);
+  
+  const res = await fetch(
+    `${process.env.TMDB_API_BASE_PATH}/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`
+  )
+  const topmovie= await res.json();
+  console.log('query topmovie', topmovie)
+
+  const resp = await fetch(
+    `${process.env.TMDB_API_BASE_PATH}/tv/top_rated?api_key=${process.env.TMDB_API_KEY}`
+  )
+  const toptv= await resp.json();
+  console.log('query topmovie', toptv)
 
   return {
     props: {
       trendings,
+      topmovie,
+      toptv
     },
   };
 }
