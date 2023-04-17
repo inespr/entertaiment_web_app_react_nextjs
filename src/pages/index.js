14a220ca644fea7fe4e03cc2158564f76a0c5279
inespr <iperrom@g.educaand.es>
2023-04-17 09:38:46 +0200

    Add getstaticprops to get the trending

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,31 @@
+import Trending from "@/components/Trending";
 import AppLayout from "@/layouts/AppLayout";
 
-export default function Home() {
+function Home({ trendings }) {
   return (
     <AppLayout
       metaTitle="Entertainment Web App | Home"
       metaDescription="Frontenmentor project realised by...."
-    ></AppLayout>
+    >
+      <Trending trendings={trendings} />
+    </AppLayout>
   );
 }
+
+export async function getStaticProps() {
+  const media_type = "all";
+  const time_window = "week";
+  const response = await fetch(
+    `${process.env.TMDB_API_BASE_PATH}/trending/${media_type}/${time_window}?api_key=${process.env.TMDB_API_KEY}`
+  );
+  const trendings = await response.json();
+  console.log("query trending", trendings);
+
+  return {
+    props: {
+      trendings,
+    },
+  };
+}
+
+export default Home;
