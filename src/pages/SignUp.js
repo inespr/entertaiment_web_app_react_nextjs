f33fda4e40a8bb99ac31bea8ecaee5ef8c9ef363
inespr <iperrom@g.educaand.es>
2023-04-19 12:23:25 +0200

    Add GoogleOAuthprovider

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,12 @@
 import AuthLayout from "@/layouts/AuthLayout";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function SignUp() {
-  return <AuthLayout></AuthLayout>;
+  return (
+    <div>
+      <GoogleOAuthProvider clientId="545527913001-qqrvp8anlokq45nolp1hnfirf3gq140e.apps.googleusercontent.com">
+        <AuthLayout />
+      </GoogleOAuthProvider>
+    </div>
+  );
 }
