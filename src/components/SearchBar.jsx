0f602f40897a7b2587b0af7048304e8b2985ed0c
inespr <iperrom@g.educaand.es>
2023-04-19 12:31:15 +0200

    Create fetchData to get the searchs movies

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,29 @@
-import Image from 'next/image'
-import styles from './SearchBar.module.css'
+import Image from "next/image";
+import styles from "./SearchBar.module.css";
+import { useEffect, useState, } from "react";
+import { useDataContext } from "@/contexts/dataContext";
 
 export default function SearchBar() {
+  //encodeURI(query)
+  const [data, setData] = useState({})
+  const [query, setQuery] = useState("");
+  //const {setContextData} = useDataContext();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_TMDB_API_BASE_PATH}/search/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+      );
+      const data = await res.json();
+      console.log(data);
+      setData(data)
+      setContextData(data)
+    };
+    if (query.length >= 3) {
+      fetchData();
+    }
+  }, [query]);
+ 
   return (
     <div className={styles.input_search_wrapper}>
       <Image
@@ -11,7 +33,15 @@ export default function SearchBar() {
         height={15}
         className={styles.svg}
       />
-      <input type="text" className={styles.input_search} placeholder="Search for movies, series or famous person" />
+      <input
+        type="search"
+        className={styles.input_search}
+        placeholder="Search for movies, series or famous person"
+        minLength={3}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+
     </div>
-  )
+  );
 }
+
