9c99c62997de7c84c353ba44a806bcf7719ac2e7
inespr <iperrom@g.educaand.es>
2023-04-19 12:30:03 +0200

    Change nambre of environment variables

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,26 @@
+import { SearchResults } from "@/components/SearchResults";
 import TopRated from "@/components/Top_rated";
 import Trending from "@/components/Trending";
+import { DataContextProvider, useDataContext } from "@/contexts/dataContext";
 import AppLayout from "@/layouts/AppLayout";
 
-function Home({ trendings, topmovie, toptv }) {
+function Home({ trendings, topmovie, toptv, search }) {
+  //const {contextData} = useDataContext();
+
   return (
-    <AppLayout
-      metaTitle="Entertainment Web App | Home"
-      metaDescription="Frontenmentor project realised by...."
-    >
-      <Trending trendings={trendings} />
-      <TopRated topmovie={topmovie} toptv={toptv}/>
-    </AppLayout>
+    //<DataContextProvider>
+      <AppLayout
+        metaTitle="Entertainment Web App | Home"
+        metaDescription="Frontenmentor project realised by...."
+      >
+        <SearchResults></SearchResults>
+        <Trending trendings={trendings} />
+        <TopRated topmovie={topmovie} toptv={toptv} />
+
+        {/*contextData ? <SearchResults></SearchResults> : <><Trending trendings={trendings} /><TopRated topmovie={topmovie} toptv={toptv} /></>*/}
+      </AppLayout>
+
+    //</DataContextProvider>
   );
 }
 
@@ -18,28 +28,28 @@ export async function getStaticProps() {
   const media_type = "all";
   const time_window = "week";
   const response = await fetch(
-    `${process.env.TMDB_API_BASE_PATH}/trending/${media_type}/${time_window}?api_key=${process.env.TMDB_API_KEY}`
+    `${process.env.NEXT_PUBLIC_TMDB_API_BASE_PATH}/trending/${media_type}/${time_window}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
   );
   const trendings = await response.json();
   console.log("query trending", trendings);
-  
+
   const res = await fetch(
-    `${process.env.TMDB_API_BASE_PATH}/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`
-  )
-  const topmovie= await res.json();
-  console.log('query topmovie', topmovie)
+    `${process.env.NEXT_PUBLIC_TMDB_API_BASE_PATH}/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+  );
+  const topmovie = await res.json();
+  console.log("query topmovie", topmovie);
 
   const resp = await fetch(
-    `${process.env.TMDB_API_BASE_PATH}/tv/top_rated?api_key=${process.env.TMDB_API_KEY}`
-  )
-  const toptv= await resp.json();
-  console.log('query topmovie', toptv)
+    `${process.env.NEXT_PUBLIC_TMDB_API_BASE_PATH}/tv/top_rated?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+  );
+  const toptv = await resp.json();
+  console.log("query topmovie", toptv);
 
   return {
     props: {
       trendings,
       topmovie,
-      toptv
+      toptv,
     },
   };
 }
