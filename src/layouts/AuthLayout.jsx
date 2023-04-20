becbc90cc387690cf14d68c799beafe22645a238
Inés <102217833+inespr@users.noreply.github.com>
2023-04-20 13:21:30 +0000

    Fix style authlayout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,9 +1,10 @@
 import SignUp from "@/components/SignUp";
+import styles from './AuthLayout.module.css'
 
 export default function AuthLayout() {
   return (
-    <>
+    <section className={styles.authLayout}>
       <SignUp />
-    </>
+    </section>
   );
 }
