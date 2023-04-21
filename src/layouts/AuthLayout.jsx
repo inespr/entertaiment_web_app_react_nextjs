203f1cb11d047df12ea7b7b86770c42b7fe0f472
Inés <102217833+inespr@users.noreply.github.com>
2023-04-21 09:48:43 +0000

    Add the google buttom font

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,10 +1,21 @@
 import SignUp from "@/components/SignUp";
-import styles from './AuthLayout.module.css'
+import styles from "./AuthLayout.module.css";
+import Head from "next/head";
 
 export default function AuthLayout() {
   return (
-    <section className={styles.authLayout}>
-      <SignUp />
-    </section>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <section className={styles.authLayout}>
+        <SignUp />
+      </section>
+    </>
   );
 }
