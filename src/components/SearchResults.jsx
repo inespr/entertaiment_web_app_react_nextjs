d8348522941bbacd0baab0a04c0b5ad4403dcfc2
Inés <102217833+inespr@users.noreply.github.com>
2023-04-25 08:07:56 +0000

    Print the results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,4 +1,17 @@
+import { useContext } from "react";
+import SearchBar, { SearchContext } from "./SearchBar";
+import { SearchDataContext } from "./DataContext";
 
-export function SearchResults(params) {
-  
+export function SearchResults() {
+  const value = useContext(SearchDataContext);
+  console.log("datos en search result", value.results);
+  return (
+    <>
+      {value && value.results && value.results.map((result) => (
+        <li key={result.id}>
+          {result.title ? result.title : result.name}
+        </li>
+      ))}
+    </>
+  );
 }
