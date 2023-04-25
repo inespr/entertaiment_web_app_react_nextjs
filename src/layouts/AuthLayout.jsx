fda3f23d19a74aa17d616505e762c8cf77b2c56e
Inés <102217833+inespr@users.noreply.github.com>
2023-04-25 08:11:44 +0000

    Remove the GoogleFont Link

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -8,10 +8,7 @@ export default function AuthLayout() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
-          rel="stylesheet"
-        />
+        
       </Head>
       <section className={styles.authLayout}>
         <SignUp />
