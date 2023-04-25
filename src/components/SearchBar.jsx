9392fe9e01eeb74418475b1ad8e1bf65846144df
Inés <102217833+inespr@users.noreply.github.com>
2023-04-25 08:07:31 +0000

    Add context to get the Data

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,47 +1,51 @@
 import Image from "next/image";
+import { createContext, useContext } from "react";
 import styles from "./SearchBar.module.css";
-import { useEffect, useState, } from "react";
-import { useDataContext } from "@/contexts/dataContext";
+import { useEffect, useState } from "react";
+import { SearchDataContext } from "./DataContext";
+import { SearchResults } from "./SearchResults";
 
 export default function SearchBar() {
-  //encodeURI(query)
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
   const [query, setQuery] = useState("");
-  //const {setContextData} = useDataContext();
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_TMDB_API_BASE_PATH}/search/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+        `${process.env.NEXT_PUBLIC_TMDB_API_BASE_PATH}/search/movie?api_key=${
+          process.env.NEXT_PUBLIC_TMDB_API_KEY
+        }&language=en-US&query=${encodeURI(query)}&page=1&include_adult=false`
       );
       const data = await res.json();
       console.log(data);
-      setData(data)
-      setContextData(data)
+      setData(data);
     };
     if (query.length >= 3) {
       fetchData();
     }
   }, [query]);
- 
-  return (
-    <div className={styles.input_search_wrapper}>
-      <Image
-        src="/icons/icon-search.svg"
-        alt="movies"
-        width={15}
-        height={15}
-        className={styles.svg}
-      />
-      <input
-        type="search"
-        className={styles.input_search}
-        placeholder="Search for movies, series or famous person"
-        minLength={3}
-        onChange={(event) => setQuery(event.target.value)}
-      />
 
-    </div>
+  return (
+    <>
+      <div className={styles.input_search_wrapper}>
+        <Image
+          src="/icons/icon-search.svg"
+          alt="movies"
+          width={15}
+          height={15}
+          className={styles.svg}
+        />
+        <input
+          type="search"
+          className={styles.input_search}
+          placeholder="Search for movies, series or famous person"
+          minLength={3}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </div>
+      <SearchDataContext.Provider value={data}>
+        <SearchResults />
+      </SearchDataContext.Provider>
+    </>
   );
 }
-
